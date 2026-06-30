8be84d7f6779c6f2234db85a26e1777603b20dca
EhsanRzi <enrforcf@gmail.com>
2026-06-30 14:38:13 +0330

    adder turing machine

--- tla-project/Part 1 - Busy Beaver/test_turing_adder.py
+++ tla-project/Part 1 - Busy Beaver/test_turing_adder.py
@@ -1,24 +1,16 @@
-# -*- coding: utf-8 -*-
+
 from turing_machine import TuringMachine
+from test_turing_machine_example1 import print_states
 
 transitions = {
-    # Phase 1: Look for the separator '0'
-    ('q0', '1'): ('q0', '1', 'R'),
-    ('q0', '0'): ('find_end', '1', 'R'),  # Replace 0 with 1
+        ('q0', '1'): ('q0', '1', 'R'),
+        ('q0', '0'): ('find_end', '1', 'R'),
     
-    # Phase 2: Go to the end of the combined string
-    ('find_end', '1'): ('find_end', '1', 'R'),
-    ('find_end', ''): ('remove_extra', '', 'L'),
+        ('find_end', '1'): ('find_end', '1', 'R'),
+        ('find_end', ''): ('remove_extra', '', 'L'),
     
-    # Phase 3: Remove the last 1 to correct the count (n+m)
-    ('remove_extra', '1'): ('qa', '', 'R'),
+        ('remove_extra', '1'): ('qa', '', 'R'),
 }
-
-if __name__ == "__main__":
-    machine = TuringMachine(transitions, blank_symbol='')
-    input_str = "110111" # 2 + 3
-    print(f"Running Addition on {input_str}...")
-    machine.debug(input_str)
 
 if __name__ == "__main__":
     print_states(transitions)
@@ -31,12 +23,9 @@ if __name__ == "__main__":
         machine.debug(w)
         print()
 
-    # SHOULD ACCEPT
+    
     run("110111")
-    # outputs 11111
-
-    # SHOULD ACCEPT
+    
     run("11101111")
-    #     # outputs 1111111
+
     run("0111")
-    # outputs 111
