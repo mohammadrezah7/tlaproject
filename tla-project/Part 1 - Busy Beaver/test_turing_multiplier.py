42ddcd70125e0b99424476aad231a2d283f82a67
EhsanRzi <enrforcf@gmail.com>
2026-06-30 16:19:03 +0330

    turing machine multiplier impliment

--- tla-project/Part 1 - Busy Beaver/test_turing_multiplier.py
+++ tla-project/Part 1 - Busy Beaver/test_turing_multiplier.py
@@ -1,44 +1,60 @@
-# -*- coding: utf-8 -*-
+
 from turing_machine import TuringMachine
+from test_turing_machine_example1 import print_states
 
 transitions = {
-    # Start: Mark a '1' in group 1 with 'X'
-    ('q0', '1'): ('find_group2', 'X', 'R'),
-    ('q0', '0'): ('cleanup', '', 'R'), # Done marking group 1
+    
+    ('q0', '1'): ('q1', 'X', 'R'),
+    ('q0', '0'): ('q7', '0', 'L'),
+       
+    ('q1', '1'): ('q1', '1', 'R'),
+    ('q1', '0'): ('q2', '0', 'R'),
+    
+    ('q2', '1'): ('q2', 'Y', 'R'),
+    ('q2', ''): ('q3', '', 'L'),
+    ('q2','Z'): ('q3', 'Z','L'),
+       
+    ('q3', 'Y'): ('q4', '1', 'R'),
 
-    # Find the start of group 2
-    ('find_group2', '1'): ('find_group2', '1', 'R'),
-    ('find_group2', '0'): ('copy_loop', '0', 'R'),
+    ('q4', 'Z'): ('q4', 'Z', 'R'),
+    ('q4', '1'): ('q4', '1', 'R'),
+    ('q4', ''): ('q5', 'Z', 'L'),
 
-    # Inside group 2: Mark a '1' with 'Y', then go to far end to add a '1'
-    ('copy_loop', '1'): ('to_far_end', 'Y', 'R'),
-    ('copy_loop', '0'): ('reset_group2', '0', 'L'), # Group 2 fully copied for this X
-    ('copy_loop', ''): ('reset_group2', '', 'L'), 
+    ('q5', 'Y'): ('q4', '1', 'R'),
+    ('q5', 'Z'): ('q5', 'Z', 'L'),
+    ('q5', '1'): ('q5', '1', 'L'),
+    ('q5', '0'):  ('q6', '0', 'L'),
 
-    # Move to the very end of the tape to write the result '1'
-    ('to_far_end', '1'): ('to_far_end', '1', 'R'),
-    ('to_far_end', '0'): ('to_far_end', '0', 'R'),
-    ('to_far_end', ''): ('back_to_group2', '1', 'L'), # Write '1' at end
+    ('q6', '1'): ('q6', '1', 'L'),
+    ('q6', 'X'): ('q0', 'X', 'R'),
 
-    # Move back to find the next 'Y' in group 2
-    ('back_to_group2', '1'): ('back_to_group2', '1', 'L'),
-    ('back_to_group2', '0'): ('back_to_group2', '0', 'L'),
-    ('back_to_group2', 'Y'): ('copy_loop', 'Y', 'R'), # Resume copying
+    ('q7', 'X'): ('q7', 'X', 'L'),
+    ('q7', ''): ('q8', '', 'R'),
 
-    # Reset Y markers to 1 and return to find next '1' in group 1
-    ('reset_group2', 'Y'): ('reset_group2', '1', 'L'),
-    ('reset_group2', '1'): ('reset_group2', '1', 'L'),
-    ('reset_group2', '0'): ('reset_group2', '0', 'L'),
-    ('reset_group2', 'X'): ('q0', 'X', 'R'), # Look for next '1'
+    ('q8', '1'): ('q8', '', 'R'),   
+    ('q8', 'Z'): ('q8', '1', 'R'),   
+    ('q8', 'X'): ('q8', '', 'R'),   
+    ('q8', 'Y'): ('q8', '', 'R'),   
+    ('q8', '0'): ('q8', '', 'R'),   
+    ('q8',''): ('qa','','S')
 
-    # Cleanup: Erase everything before the result string
-    ('cleanup', '1'): ('cleanup', '', 'R'),
-    ('cleanup', '0'): ('cleanup', '', 'R'),
-    ('cleanup', ''): ('qa', '', 'R'),
 }
 
 if __name__ == "__main__":
-    machine = TuringMachine(transitions, blank_symbol='')
-    input_str = "110111" # 2 * 3
-    print(f"Running Multiplication on {input_str}...")
-    machine.debug(input_str, step_limit=500)
+    print_states(transitions)
+    machine = TuringMachine(transitions)
+
+    def run(input_):
+        w = input_
+        print("Input:",w)
+        print("Accepted" if machine.accepts(w) else "Rejected")
+        machine.debug(w, step_limit=1000)
+
+        print()
+
+    
+    run("1110111")
+    
+    run("11101111")
+
+    run("01111")
