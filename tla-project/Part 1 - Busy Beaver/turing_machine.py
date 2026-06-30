4ef0e490ce3bfceea7f32cff65f2c42a199a2966
EhsanRzi <enrforcf@gmail.com>
2026-06-30 14:33:07 +0330

    turing maching impliment fixed

--- tla-project/Part 1 - Busy Beaver/turing_machine.py
+++ tla-project/Part 1 - Busy Beaver/turing_machine.py
@@ -115,38 +115,3 @@ class TuringMachine:
                 output += f" -> {action}"
             print(output)
 
-new1 = TuringMachine(transitions = {
-        ('q0', '#'): ('End', '#', 'R'),
-        ('End', ''): ('qa', '', 'R'),
-
-        ('q0', '0'): ('FindDelimiter0', 'X', 'R'),
-        ('FindDelimiter0', '#'): ('Check0', '#', 'R'),
-        ('Check0', '0'): ('FindLeftmost', 'X', 'L'),
-
-        ('q0', '1'): ('FindDelimiter1', 'X', 'R'),
-        ('FindDelimiter1', '#'): ('Check1', '#', 'R'),
-        ('Check1', '1'): ('FindLeftmost', 'X', 'L'),
-
-        ('FindLeftmost', '0'): ('FindLeftmost', '0', 'L'),
-        ('FindLeftmost', '1'): ('FindLeftmost', '1', 'L'),
-        ('FindLeftmost', 'X'): ('FindLeftmost', 'X', 'L'),
-        ('FindLeftmost', '#'): ('FindLeftmost', '#', 'L'),
-        ('FindLeftmost', ''): ('FindNext', '', 'R'),
-
-        ('FindNext', 'X'): ('FindNext', 'X', 'R'),
-        ('FindNext', '0'): ('FindDelimiter0', 'X', 'R'),
-        ('FindNext', '1'): ('FindDelimiter1', 'X', 'R'),
-        ('FindNext', '#'): ('End', '#', 'R'),
-
-        ('FindDelimiter0', '0'): ('FindDelimiter0', '0', 'R'),
-        ('FindDelimiter0', '1'): ('FindDelimiter0', '1', 'R'),
-        ('FindDelimiter1', '0'): ('FindDelimiter1', '0', 'R'),
-        ('FindDelimiter1', '1'): ('FindDelimiter1', '1', 'R'),
-
-        ('Check0', 'X'): ('Check0', 'X', 'R'),
-        ('Check1', 'X'): ('Check1', 'X', 'R'),
-
-        ('End', 'X'): ('End', 'X', 'R')
-})
-
-new1.accepts("11#XXXX1")
